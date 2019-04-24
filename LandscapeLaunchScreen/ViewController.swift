d79af838c8d4444721282fa8179266b7d1a035c4
Jochen Pfeiffer <git@jochen-pfeiffer.com>
2019-04-24 08:54:36 +0200

    Initial Commit

---
+++ LandscapeLaunchScreen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LandscapeLaunchScreen
+//
+//  Created by Jochen on 24.04.19.
+//  Copyright © 2019 Jochen Pfeiffer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
